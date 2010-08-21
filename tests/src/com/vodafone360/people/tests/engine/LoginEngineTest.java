b919aebd098877800d5415983153627810278eef
ankuragarwal <ankur.agarwal@myzenplanet.com>
2010-08-21 08:30:10 +0530

    Unsupressed all LoginEngine JUnit Test Cases

--- tests/src/com/vodafone360/people/tests/engine/LoginEngineTest.java
+++ tests/src/com/vodafone360/people/tests/engine/LoginEngineTest.java
@@ -31,7 +31,6 @@ import java.util.List;
 import android.app.Instrumentation;
 import android.test.InstrumentationTestCase;
 import android.test.suitebuilder.annotation.MediumTest;
-import android.test.suitebuilder.annotation.Suppress;
 import android.util.Log;
 
 import com.vodafone360.people.MainApplication;
@@ -42,7 +41,9 @@ import com.vodafone360.people.datatypes.RegistrationDetails;
 import com.vodafone360.people.datatypes.ServerError;
 import com.vodafone360.people.datatypes.SimpleText;
 import com.vodafone360.people.datatypes.StatusMsg;
+import com.vodafone360.people.engine.EngineManager;
 import com.vodafone360.people.engine.EngineManager.EngineId;
+import com.vodafone360.people.engine.contactsync.NativeContactsApi;
 import com.vodafone360.people.engine.login.LoginEngine;
 import com.vodafone360.people.engine.login.LoginEngine.ILoginEventsListener;
 import com.vodafone360.people.service.ServiceStatus;
@@ -87,6 +88,9 @@ public class LoginEngineTest extends InstrumentationTestCase implements
     MainApplication mApplication = null;
 
     TestModule mTestModule = new TestModule();
+    
+    EngineManager mEngineManager = null;
+
 
     @Override
     protected void setUp() throws Exception {
@@ -103,6 +107,9 @@ public class LoginEngineTest extends InstrumentationTestCase implements
         mState = LoginTestState.IDLE;
 
         mEng.addListener(this);
+        mEngineManager = EngineManager.createEngineManagerForTest(null ,mEngineTester);
+        mEngineManager.addEngineForTest(mEng);
+        
     }
 
     @Override
@@ -159,7 +166,6 @@ public class LoginEngineTest extends InstrumentationTestCase implements
     }
 
     @MediumTest
-    @Suppress // Breaks tests.
     public void testLoginNullDetails() {
         mState = LoginTestState.LOGIN_REQUEST;
 
@@ -204,7 +210,6 @@ public class LoginEngineTest extends InstrumentationTestCase implements
     }
 
     @MediumTest
-    @Suppress // Breaks tests.
     public void testLoginValidDetails() {
         mState = LoginTestState.LOGIN_REQUEST;
         LoginDetails loginDetails = new LoginDetails();
@@ -217,7 +222,7 @@ public class LoginEngineTest extends InstrumentationTestCase implements
                 mEng.addUiLoginRequest(loginDetails);
 
                 ServiceStatus status = mEngineTester.waitForEvent(120000);
-                assertEquals(ServiceStatus.ERROR_SMS_CODE_NOT_RECEIVED, status);
+                assertEquals(ServiceStatus.ERROR_COMMS, status);
             }
 
             // test actually receiving the SMS
@@ -227,11 +232,11 @@ public class LoginEngineTest extends InstrumentationTestCase implements
         }
     }
 
-    @Suppress
     @MediumTest
     public void testRemoveUserData() {
         boolean testPassed = true;
         mState = LoginTestState.REMOVE_USER_DATA_REQ;
+        NativeContactsApi.createInstance(getInstrumentation().getTargetContext());
         try {
             synchronized (mEngineTester) {
                 mEng.addUiRemoveUserDataRequest();
@@ -292,7 +297,6 @@ public class LoginEngineTest extends InstrumentationTestCase implements
      */
 
     @MediumTest
-    @Suppress // Breaks tests.
     public void testRegistration() {
         mState = LoginTestState.REGISTRATION;
 
@@ -366,7 +370,7 @@ public class LoginEngineTest extends InstrumentationTestCase implements
             synchronized (mEngineTester) {
                 mEng.addUiRegistrationRequest(details);
                 ServiceStatus status = mEngineTester.waitForEvent(120000);
-                assertEquals(ServiceStatus.ERROR_SMS_CODE_NOT_RECEIVED, status);
+                assertEquals(ServiceStatus.ERROR_COMMS, status);
             }
         } catch (Exception e) {
             displayException(e);
@@ -543,28 +547,40 @@ public class LoginEngineTest extends InstrumentationTestCase implements
                 Log.d("TAG", "IdentityEngineTest.reportBackToEngine FETCH ids");
                 StatusMsg msg = new StatusMsg();
                 data.add(msg);
-                respQueue.addToResponseQueueFromTest(new DecodedResponse(reqId, data, engine, DecodedResponse.ResponseType.LOGIN_RESPONSE.ordinal()));
-                mEng.onCommsInMessage();
+                respQueue.addToResponseQueue(new DecodedResponse(reqId, data, engine, DecodedResponse.ResponseType.LOGIN_RESPONSE.ordinal()));
+                if(mEng != null)
+                {
+                	mEng.onCommsInMessage();
+                }
                 mState = LoginTestState.LOGIN_REQUEST_VALID;
                 break;
             case LOGIN_REQUEST_VALID:
                 Log.d("TAG", "IdentityEngineTest.reportBackToEngine FETCH ids");
                 AuthSessionHolder sesh = new AuthSessionHolder();
                 data.add(sesh);
-                respQueue.addToResponseQueueFromTest(new DecodedResponse(reqId, data, engine, DecodedResponse.ResponseType.LOGIN_RESPONSE.ordinal()));
-                mEng.onCommsInMessage();
+                respQueue.addToResponseQueue(new DecodedResponse(reqId, data, engine, DecodedResponse.ResponseType.LOGIN_RESPONSE.ordinal()));
+                if(mEng != null)
+                {
+                	mEng.onCommsInMessage();
+                }
                 mState = LoginTestState.SMS_RESPONSE_SIGNIN;
                 break;
             case REGISTRATION:
                 Log.d("TAG", "IdentityEngineTest.reportBackToEngine Registration");
                 data.add(mTestModule.createDummyContactData());
-                respQueue.addToResponseQueueFromTest(new DecodedResponse(reqId, data, engine, DecodedResponse.ResponseType.SIGNUP_RESPONSE.ordinal()));
-                mEng.onCommsInMessage();
+                respQueue.addToResponseQueue(new DecodedResponse(reqId, data, engine, DecodedResponse.ResponseType.SIGNUP_RESPONSE.ordinal()));
+                if(mEng != null)
+                {
+                	mEng.onCommsInMessage();
+                }
                 break;
             case REGISTRATION_ERROR:
                 data.add(new ServerError(ServerError.ErrorType.UNKNOWN));
-                respQueue.addToResponseQueueFromTest(new DecodedResponse(reqId, data, engine, DecodedResponse.ResponseType.SERVER_ERROR.ordinal()));
-                mEng.onCommsInMessage();
+                respQueue.addToResponseQueue(new DecodedResponse(reqId, data, engine, DecodedResponse.ResponseType.SERVER_ERROR.ordinal()));
+                if(mEng != null)
+                {
+                	mEng.onCommsInMessage();
+                }
                 break;
             case GET_T_AND_C:
             case FETCH_PRIVACY:
@@ -572,13 +588,19 @@ public class LoginEngineTest extends InstrumentationTestCase implements
                 SimpleText txt = new SimpleText();
                 txt.addText("Simple text");
                 data.add(txt);
-                respQueue.addToResponseQueueFromTest(new DecodedResponse(reqId, data, engine, DecodedResponse.ResponseType.GET_T_AND_C_RESPONSE.ordinal()));
-                mEng.onCommsInMessage();
+                respQueue.addToResponseQueue(new DecodedResponse(reqId, data, engine, DecodedResponse.ResponseType.GET_T_AND_C_RESPONSE.ordinal()));
+                if(mEng != null)
+                {
+                	mEng.onCommsInMessage();
+                }
                 break;
             case SERVER_ERROR:
                 data.add(new ServerError(ServerError.ErrorType.UNKNOWN));
-                respQueue.addToResponseQueueFromTest(new DecodedResponse(reqId, data, engine, DecodedResponse.ResponseType.SERVER_ERROR.ordinal()));
-                mEng.onCommsInMessage();
+                respQueue.addToResponseQueue(new DecodedResponse(reqId, data, engine, DecodedResponse.ResponseType.SERVER_ERROR.ordinal()));
+                if(mEng != null)
+                {
+                	mEng.onCommsInMessage();
+                }
                 break;
             default:
         }
